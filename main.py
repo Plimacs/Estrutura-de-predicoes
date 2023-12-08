b87e5d2600b9816f45e390d61c206c029e4aff14
Matheus Pereira Lima <132246877+Plimacs@users.noreply.github.com>
2023-12-08 18:37:35 -0300

    Correção de bug #6

--- main.py
+++ main.py
@@ -3,10 +3,11 @@ from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_absolute_error, mean_squared_error
 from datetime import datetime
+import matplotlib.pyplot as plt
 
-# Constantes para definir o intervalo de datas desejado
-data_inicio = '2023-01-01'
-data_fim = '2023-10-30'
+# Constantes para definir o intervalo de datas desejado (filtro de intervalo de datas)
+#data_inicio = '2023-01-01'
+#data_fim = '2023-10-30'
 
 # Carregar os dados
 dolar = pd.read_csv('Dados/BRL=X.csv')
@@ -31,12 +32,12 @@ ouro = ouro.sort_values(by='Data')
 petroleo['Data'] = petroleo['Data'].apply(lambda x: datetime.strptime(str(x), "%d%m%Y").strftime('%Y-%m-%d') if len(str(x)) == 8 else None)
 ouro['Data'] = ouro['Data'].apply(lambda x: datetime.strptime(str(x), "%d%m%Y").strftime('%Y-%m-%d') if len(str(x)) == 8 else None)
 
-# Filtrar os dados no intervalo de datas desejado
-dolar = dolar[(dolar['Data'] >= data_inicio) & (dolar['Data'] <= data_fim)]
-bvsp = bvsp[(bvsp['Data'] >= data_inicio) & (bvsp['Data'] <= data_fim)]
-gol = gol[(gol['Data'] >= data_inicio) & (gol['Data'] <= data_fim)]
-petroleo = petroleo[(petroleo['Data'] >= data_inicio) & (petroleo['Data'] <= data_fim)]
-ouro = ouro[(ouro['Data'] >= data_inicio) & (ouro['Data'] <= data_fim)]
+# Filtrar os dados no intervalo de datas desejado (filtro de intervalo de datas)
+#dolar = dolar[(dolar['Data'] >= data_inicio) & (dolar['Data'] <= data_fim)]
+#bvsp = bvsp[(bvsp['Data'] >= data_inicio) & (bvsp['Data'] <= data_fim)]
+#gol = gol[(gol['Data'] >= data_inicio) & (gol['Data'] <= data_fim)]
+#petroleo = petroleo[(petroleo['Data'] >= data_inicio) & (petroleo['Data'] <= data_fim)]
+#ouro = ouro[(ouro['Data'] >= data_inicio) & (ouro['Data'] <= data_fim)]
 
 # Mesclar os dados em um único DataFrame usando a coluna 'Data' como chave
 merged_data = pd.merge(gol, dolar[['Data', 'Dolar_Close']], on='Data', how='inner')
@@ -44,7 +45,7 @@ merged_data = pd.merge(merged_data, petroleo[['Data', 'Ultimo']], on='Data', how
 merged_data = pd.merge(merged_data, ouro[['Data', 'Ultimo']], on='Data', how='inner')
 merged_data = pd.merge(merged_data, bvsp[['Data', 'BVSP_Close']], on='Data', how='inner')
 
-# Remover linhas com valores nulos, se houver
+# Remover linhas com valores nulos
 merged_data = merged_data.dropna()
 
 # Dividir os dados em características (X) e alvo (y)
@@ -52,7 +53,7 @@ X = merged_data[['Dolar_Close', 'Ultimo_x', 'Ultimo_y', 'BVSP_Close']]
 y = merged_data['Gol_Close']
 
 # Dividir os dados em conjunto de treinamento e teste
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=42)
 
 # Criar o modelo de regressão linear
 model = LinearRegression()
@@ -61,11 +62,26 @@ model = LinearRegression()
 model.fit(X_train, y_train)
 
 # Fazer predições no conjunto de teste
-predictions = model.predict(X_test)
+predict = model.predict(X_test)
 
 # Avaliar o desempenho do modelo
-mean_absolute_error = mean_absolute_error(y_test, predictions)
-mean_squared_error = mean_squared_error(y_test, predictions, squared=False)
+print(f'Mean Absolute Error: {mean_absolute_error(y_test, predict)}')
+print(f'Root Mean Squared Error: {mean_squared_error(y_test, predict, squared=False)}')
 
-print(f'Mean Absolute Error: {mean_absolute_error}')
-print(f'Root Mean Squared Error: {mean_squared_error}')
+# Plotando gráfico para dados de treinamento
+plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.scatter(y_train, model.predict(X_train), color='blue')
+plt.title('Previsões vs. Valores Reais (Treinamento)')
+plt.xlabel('Valores Reais')
+plt.ylabel('Previsões')
+
+# Plotando gráfico para dados de teste
+plt.subplot(1, 2, 2)
+plt.scatter(y_test, predict, color='red')
+plt.title('Previsões vs. Valores Reais (Teste)')
+plt.xlabel('Valores Reais')
+plt.ylabel('Previsões')
+
+plt.tight_layout()
+plt.show()
